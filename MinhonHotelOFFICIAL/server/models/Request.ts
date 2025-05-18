917d531763e8ef1f2b2ae1472036ca096ab4ebb3
Tên GitHub của bạn <Email GitHub ca bn>
2025-05-18 14:54:02 +0700

    cập nhật

--- MinhonHotelOFFICIAL/server/models/Request.ts
+++
@@ -1,9 +0,0 @@
-export interface Request {
-  id: number;
-  room_number: string;
-  guestName: string;
-  request_content: string;
-  created_at: Date;
-  status: string; // Đã ghi nhận, Đang thực hiện, ...
-  notes?: string;
-} 
